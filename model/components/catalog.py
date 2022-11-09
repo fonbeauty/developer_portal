8b7254ca6c0673881913c5439536a1e604530686
Ivan Tebenkov <fonbeauty@mail.ru>
2022-11-09 23:15:57 +0300

    move selectora to constants

--- model/components/catalog.py
+++ model/components/catalog.py
@@ -6,27 +6,32 @@ from model.components.base_driver import BaseDriver
 
 class Catalog(BaseDriver):
 
+    CATALOG_TITLE = '.catalog__title'
+    SEARCH_INPUT = '.input-search'
+    CARD_RATES = 'a[href="/product/ExchangeRates"]'
+    ALL_CARDS = '.card'
+
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
 
     def catalog_title(self) -> WebElement:
-        return self.wait_element('.catalog__title')
+        return self.wait_element(self.CATALOG_TITLE)
 
     def title_text(self) -> str:
         return self.catalog_title().text
 
     def search_input(self) -> WebElement:
-        return self.wait_element('.input-search')
+        return self.wait_element(self.SEARCH_INPUT)
 
     def search_input_type_text(self, text_for_search: str) -> None:
         self.search_input().send_keys(text_for_search)
         self.search_input().submit()
 
     def card_rates(self) -> WebElement:
-        return self.wait_element('a[href="/product/ExchangeRates"]')
+        return self.wait_element(self.CARD_RATES)
 
     def all_cards(self) -> list:
-        return self.wait_elements('.card')
+        return self.wait_elements(self.ALL_CARDS)
 
     def count_cards(self) -> int:
         return len(self.all_cards())
--- model/components/main.py
+++ model/components/main.py
@@ -6,17 +6,20 @@ from model.components.base_driver import BaseDriver
 
 class Main(BaseDriver):
 
+    PROFILE_LINK = '.profile__link'
+    CATALOG_LINK = 'a[href="/catalog"].text-link'
+
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
 
     def profile_link(self) -> WebElement:
-        return self.wait_element('.profile__link')
+        return self.wait_element(self.PROFILE_LINK)
 
     def profile_link_text(self) -> str:
         return self.profile_link().text
 
     def catalog_link(self) -> WebElement:
-        return self.wait_element('a[href="/catalog"].text-link')
+        return self.wait_element(self.CATALOG_LINK)
 
     def catalog_link_click(self) -> None:
         self.catalog_link().click()
